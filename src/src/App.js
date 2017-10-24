0f44156dd06c63f2b000e8307fd9d489a71b67fc
Shivan Modha <shivanmodha@gmail.com>
2017-10-24 18:23:14 -0400

    Firebase Integration

--- src/src/App.js
+++ src/src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import configuration from './configuration.json'
+class App extends Component
+{
+    constructor()
+    {
+        super();
+        /**
+         * Binding
+         */
+        this.firebaseInitialize = this.firebaseInitialize.bind(this);
+        this.firebaseSetData = this.firebaseSetData.bind(this);
+        this.firebaseGetData = this.firebaseGetData.bind(this);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+        this.firebaseInitialize(configuration);
+        this.firebaseGetData("/private/", (e) =>
+        {
+            console.log(e);
+        });
+    }
+    firebaseInitialize(configuration)
+    {
+        window.dispatchEvent(new CustomEvent("_event_onInitializeFirebase", { detail: { configuration: configuration } }));
+    }
+    firebaseSetData(reference, data)
+    {
+        window.dispatchEvent(new CustomEvent("_event_onSetData", { detail: { reference: reference, data: data } }));
+    }
+    firebaseGetData(reference, callback)
+    {
+        window.dispatchEvent(new CustomEvent("_event_onGetData", { detail: { reference: reference, callback: callback } }));
+    }
+    render()
+    {
+        return (
+            <div className="App">
+                Shivan
+            </div>
+        );
+    }
 }
-
 export default App;
