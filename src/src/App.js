eb77c0c4fae3dd92838f93090be549e99d1260b5
Shivan Modha <shivanmodha@gmail.com>
2017-10-24 21:57:00 -0400

    Set Data

--- src/src/App.js
+++ src/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import configuration from './configuration.json';
-import { Jumbotron, Button } from 'reactstrap';
+import { ListGroupItem, Modal, ModalHeader, ModalBody, ModalFooter, Button, Input } from 'reactstrap';
 class App extends Component
 {
     constructor()
@@ -13,12 +13,13 @@ class App extends Component
         this.firebaseSetData = this.firebaseSetData.bind(this);
         this.firebaseGetData = this.firebaseGetData.bind(this);
         this.createElements = this.createElements.bind(this);
+        this.inputToggle = this.inputToggle.bind(this);
         /**
          * Listeners
          */
         let forceRefresh = () =>
         {
-            this.createElements(this.private);            
+            this.createElements(this.private);
         }
         window.addEventListener("resize", forceRefresh);
         window.addEventListener("_event_onRefresh", forceRefresh);
@@ -30,7 +31,8 @@ class App extends Component
          */
         this.setState({
             update: false,
-            elements: []
+            elements: [],
+            input: false
         });
         /**
          * Initialize Firebase
@@ -60,6 +62,10 @@ class App extends Component
         let x = 0;
         let y = 0;
         let returned = 0;
+        this.firebaseGetData("/private/", (event) =>
+        {
+            this.private = event;
+        });
         this.firebaseGetData("/public/cells/", (rawData) =>
         {
             for (let i = 0; i < e.cell.max; i++)
@@ -69,22 +75,45 @@ class App extends Component
                     x = 0;
                     y++;
                 }
-                let top = (y * (e.cell.height + 1)); // center factor: (window.innerHeight / 2) - ((e.cell.max / (e.cell.perRow * 2)) * (e.cell.height + 1)) + 
-                let left = (x * (e.cell.width + 1)); // center factor: (window.innerWidth / 2) - ((e.cell.perRow / 2) * (e.cell.width + 1)) +
+                let top = (y * (e.cell.height)); // center factor: (window.innerHeight / 2) - ((e.cell.max / (e.cell.perRow * 2)) * (e.cell.height + 1)) +
+                let left = (x * (e.cell.width)); // center factor: (window.innerWidth / 2) - ((e.cell.perRow / 2) * (e.cell.width + 1)) +
                 let html = "";
+                let enabled = true;
+                let epochNow = Math.round((new Date()).getTime() / 1000);
                 if (rawData[i] != null)
                 {
-                    html = rawData[i];
+                    html = rawData[i].html;
                     while (html.includes("alert("))
                     {
                         html = html.replace("alert(", "console.log(");
                     }
+                    if (epochNow <= rawData[i].time + 60)
+                    {
+                        enabled = false;
+                    }
                 }
                 returned++;
-                elements.push(<div style={{ position: "absolute", top: top, left: left, width: e.cell.width, height: e.cell.height, backgroundColor: "rgba(240, 240, 240, 1)" }} dangerouslySetInnerHTML={{ __html: html }} onClick={(e) =>
+                if (enabled)
                 {
-                    
-                }}></div>);
+                    elements.push(<ListGroupItem tag="a" action style={{ position: "absolute", top: top, left: left, width: e.cell.width, height: e.cell.height, padding: 0, userSelect: "none", cursor: "default", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: html }} onClick={(e) =>
+                    {                        
+                        let html = "";
+                        if (rawData[i])
+                        {
+                            html = rawData[i].html;
+                        }    
+                        this.setState({
+                            currentText: html,
+                            currentIndex: i,
+                            input: true
+                        })
+                        this.createElements(this.private);
+                    }}></ListGroupItem >);
+                }
+                else if (!enabled)
+                {
+                    elements.push(<ListGroupItem tag="a" disabled style={{ position: "absolute", top: top, left: left, width: e.cell.width, height: e.cell.height, padding: 0, userSelect: "none", cursor: "not-allowed", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: html }}></ListGroupItem >);
+                }
                 if (returned == e.cell.max)
                 {
                     this.setState({
@@ -96,28 +125,68 @@ class App extends Component
             }
         });
     }
+    inputToggle()
+    {
+        this.setState({
+            input: !this.state.input
+        });
+    }
     render()
     {
         let left = 0;
         let top = 0;
+        let hei = 0;
+        let wid = 0;
         if (this.private)
         {
-            left = (window.innerWidth / 2) - ((this.private.cell.perRow / 2) * (this.private.cell.width + 1));
-            top = (window.innerHeight / 2) - ((this.private.cell.max / (this.private.cell.perRow * 2)) * (this.private.cell.height + 1));
+            left = (window.innerWidth / 2) - ((this.private.cell.perRow / 2) * (this.private.cell.width));
+            top = (window.innerHeight / 2) - ((this.private.cell.max / (this.private.cell.perRow * 2)) * (this.private.cell.height));
             if (left < 0)
             {
                 left = 0;
-            }    
+            }
             if (top < 0)
             {
                 top = 0;
-            }    
-        }    
+            }
+            hei = this.private.cell.height;
+            wid = this.private.cell.width;
+        }
         return (
             <div>
                 <div style={{ position: "absolute", top: top, left: left}}>
                     {this.state.elements}
-                </div>    
+                </div>
+                <Modal isOpen={this.state.input} toggle={this.inputToggle}>
+                    <ModalHeader toggle={this.inputToggle}>Edit Cell</ModalHeader>   
+                    <ModalBody>
+                        <p className="lead" style={{ marginBottom: 0, paddingBottom: 0 }}>Preview</p>
+                        <ListGroupItem tag="a" action style={{ width: wid, height: hei, padding: 0, marginBottom: 10, userSelect: "none", cursor: "default", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: this.state.currentText }}></ListGroupItem>
+                        <p className="lead" style={{ marginBottom: 0, paddingBottom: 0 }}>Code</p>
+                        <Input type="textarea" value={this.state.currentText} name="text" placeholder="Enter HTML Code Here" onChange={(e) =>
+                        {
+                            this.setState({
+                                currentText: e.target.value
+                            });
+                        }}>
+                        </Input>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="success" outline onClick={(e) =>
+                        {                            
+                            let epochNow = Math.round((new Date()).getTime() / 1000);
+                            if (this.private.update)
+                            {
+                                this.firebaseSetData("/public/cells/" + this.state.currentIndex + "/", {
+                                    html: this.state.currentText,
+                                    time: epochNow
+                                });
+                                this.createElements(this.private);
+                            }    
+                            this.inputToggle();
+                        }}>Save</Button>
+                    </ModalFooter>
+                </Modal>    
             </div>
         );
     }
