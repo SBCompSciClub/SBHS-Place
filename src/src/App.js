0af3c0733eb7366ac6c3d0bec85765b9bd860120
Shivan Modha <shivanmodha@gmail.com>
2018-01-18 15:51:38 -0500

    Rerouting

--- src/src/App.js
+++ src/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import configuration from './configuration.json';
-import { ListGroupItem, Modal, ModalHeader, ModalBody, ModalFooter, Button, Input } from 'reactstrap';
+import { ListGroupItem, Modal, ModalHeader, ModalBody, ModalFooter, Button, Input, Form, FormGroup, Label, Progress } from 'reactstrap';
 class App extends Component
 {
     constructor()
@@ -41,6 +41,7 @@ class App extends Component
         this.firebaseGetData("/private/", (e) =>
         {
             this.private = e;
+            console.log(this.private);
             this.createElements(e);
         });
     }
@@ -95,14 +96,17 @@ class App extends Component
                 returned++;
                 if (enabled)
                 {
-                    elements.push(<ListGroupItem tag="a" action style={{ position: "absolute", top: top, left: left, width: e.cell.width, height: e.cell.height, padding: 0, userSelect: "none", cursor: "default", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: html }} onClick={(e) =>
+                    elements.push(<ListGroupItem tag="a" action style={{ position: "absolute", top: top, left: left, backgroundColor: "black", border: "1px solid rgba(255, 255, 255, 0.1)", borderRadius: 0, color: "white", width: e.cell.width, height: e.cell.height, padding: 0, userSelect: "none", cursor: "default", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: html }} onClick={(e) =>
                     {                        
                         let html = "";
+                        let value = "";
                         if (rawData[i])
                         {
                             html = rawData[i].html;
+                            value = rawData[i].value;
                         }    
                         this.setState({
+                            currentValue: value,
                             currentText: html,
                             currentIndex: i,
                             input: true
@@ -112,7 +116,7 @@ class App extends Component
                 }
                 else if (!enabled)
                 {
-                    elements.push(<ListGroupItem tag="a" disabled style={{ position: "absolute", top: top, left: left, width: e.cell.width, height: e.cell.height, padding: 0, userSelect: "none", cursor: "not-allowed", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: html }}></ListGroupItem >);
+                    elements.push(<ListGroupItem tag="a" disabled style={{ position: "absolute", top: top, left: left, backgroundColor: "black", border: "1px solid rgba(255, 255, 255, 0.1)", borderRadius: 0, color: "white", width: e.cell.width, height: e.cell.height, padding: 0, userSelect: "none", cursor: "not-allowed", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: html }}></ListGroupItem >);
                 }
                 if (returned === e.cell.max)
                 {
@@ -137,6 +141,8 @@ class App extends Component
         let top = 0;
         let hei = 0;
         let wid = 0;
+        let bpp = 12;
+        let bpp_max = 15;
         if (this.private)
         {
             left = (window.innerWidth / 2) - ((this.private.cell.perRow / 2) * (this.private.cell.width));
@@ -151,6 +157,8 @@ class App extends Component
             }
             hei = this.private.cell.height;
             wid = this.private.cell.width;
+            bpp = this.private.bpp;
+            bpp_max = this.private.bpp_max;
         }
         return (
             <div>
@@ -161,15 +169,64 @@ class App extends Component
                     <ModalHeader toggle={this.inputToggle}>Edit Cell</ModalHeader>   
                     <ModalBody>
                         <p className="lead" style={{ marginBottom: 0, paddingBottom: 0 }}>Preview</p>
-                        <ListGroupItem tag="a" action style={{ width: wid, height: hei, padding: 0, marginBottom: 10, userSelect: "none", cursor: "default", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: this.state.currentText }}></ListGroupItem>
-                        <p className="lead" style={{ marginBottom: 0, paddingBottom: 0 }}>Code</p>
-                        <Input type="textarea" value={this.state.currentText} name="text" placeholder="Enter HTML Code Here" onChange={(e) =>
-                        {
-                            this.setState({
-                                currentText: e.target.value
-                            });
-                        }}>
-                        </Input>
+                        <ListGroupItem tag="a" action style={{ backgroundColor: "black", border: "1px solid rgba(255, 255, 255, 0.1)", borderRadius: 0, color: "white", width: wid, height: hei, padding: 0, marginBottom: 10, userSelect: "none", cursor: "default", overflow: "hidden" }} dangerouslySetInnerHTML={{ __html: this.state.currentText }}></ListGroupItem>
+                        <p className="lead" style={{ marginBottom: 0, paddingBottom: 0 }}>Color</p>
+                        <Form onSubmit={(e) => { e.preventDefault(); }}>
+                            <FormGroup>
+                                <Label>Bits Per Pixel</Label>
+                                <Progress value={bpp} max={bpp_max}>{bpp}</Progress>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Bits</Label>
+                                <Input value={this.state.currentValue} name="text" placeholder="Enter Binary" onChange={(e) =>
+                                {
+                                    let value = e.target.value;
+                                    let validation = true;
+                                    let html = "<div style='color: red; width: 100%; height: 100%; text-align: center; vertical-align: middle;'>!</div>";
+                                    // Input Validation
+                                    if (value.length > bpp)
+                                    {
+                                        value = value.substring(0, bpp);
+                                    }
+                                    for (let i = 0; i < value.length; i++)
+                                    {
+                                        if (value.substring(i, i + 1) === "0" || value.substring(i, i + 1) === "1")
+                                        {
+                                        }
+                                        else
+                                        {
+                                            validation = false;
+                                        }    
+                                    }
+                                    if (validation)
+                                    {
+                                        let r = 0;
+                                        let g = 0;
+                                        let b = 0;
+                                        if (value.length === bpp)
+                                        {
+                                            let section = bpp / 3;
+                                            let m = "";
+                                            for (let i = 0; i < section; i++)
+                                            {
+                                                m += "1";
+                                            }
+                                            let max = parseInt(m, 2);
+                                            r = (parseInt(value.substring(section * 0, section * 1), 2) / max) * 255;
+                                            g = (parseInt(value.substring(section * 1, section * 2), 2) / max) * 255;
+                                            b = (parseInt(value.substring(section * 2, section * 3), 2) / max) * 255;
+                                            console.log(r + ", " + g + ", " + b);
+                                        }    
+                                        html = "<div style=\"width: 100%; height: 100%; background: rgba(" + r + ", " + g + ", " + b + ", 1);\"></div>";
+                                    }
+                                    this.setState({
+                                        currentValue: value,
+                                        currentText: html
+                                    });
+                                }}>
+                                </Input>
+                            </FormGroup>
+                        </Form>
                     </ModalBody>
                     <ModalFooter>
                         <Button color="success" outline onClick={(e) =>
@@ -179,7 +236,8 @@ class App extends Component
                             {
                                 this.firebaseSetData("/public/cells/" + this.state.currentIndex + "/", {
                                     html: this.state.currentText,
-                                    time: epochNow
+                                    time: epochNow,
+                                    value: this.state.currentValue
                                 });
                                 this.createElements(this.private);
                             }    
@@ -187,7 +245,7 @@ class App extends Component
                         }}>Save</Button>
                     </ModalFooter>
                 </Modal>                
-                <p style={{ position: "fixed", bottom: 0, right: 0, padding: 0, margin: 0, fontSize: 10 }}>Created By Shivan Modha</p>
+                <p style={{ position: "fixed", color: "rgba(255, 255, 255, 0.5)", bottom: 0, right: 0, padding: 0, margin: 0, fontSize: 10 }}>Created By Shivan Modha</p>
             </div>
         );
     }
